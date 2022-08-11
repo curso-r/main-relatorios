5b93aa1f3fc816039ac91e016b082dc8cce27c48
Beatriz Milz <milz.bea@gmail.com>
2022-08-11 14:15:23 -0300

    buscando conteudo sobre relatorios com parametros no oferecimento passado

--- praticas/aula_2/params_purrr.R
+++ praticas/aula_2/params_purrr.R
@@ -7,30 +7,54 @@ caminho_rmd <- here::here("praticas/aula_2/relatorio-completo.Rmd")
 rmarkdown::render(caminho_rmd)
 
 # posso gerar mudando o param
-rmarkdown::render(caminho_rmd, params = list(estado = "SP"))
+
+list(estado = "SP")
+
+rmarkdown::render(caminho_rmd,
+                  params = list(estado = "SP"))
 
 # posso configurar outras coisas:
 
-rmarkdown::render(caminho_rmd, params = list(estado = "SP"),
+rmarkdown::render(caminho_rmd,
+                  params = list(estado = "SP"),
                   output_file = "SP.html")
 
 # buscando UFs possíveis
 sigbm <- read_xlsx("dados/sigbm.xlsx", skip = 4) |>
   clean_names()
 
+sigbm$uf
+
+unique(sigbm$uf)
+
 estados_no_sigbm <- sort(unique(sigbm$uf))
 
 
-# purrr
+# vamos começar a falar de purrr!!
 
 library(purrr)
 # criar a pasta caso nao exista
 fs::dir_create("praticas/aula_2/relatorios_uf")
 
 # iterar!
+# exemplo curto de purr
+# 1, 2, 3
+sqrt(1)
+sqrt(2)
+sqrt(3)
 
-# exemplo de map
-map(1:3, ~sqrt(.x))
+
+vetor_que_quero_repetir <- 1:3
+
+
+purrr::map(vetor_que_quero_repetir,
+           # funcao que eu quero que seja aplicada
+           sqrt)
+
+purrr::map(vetor_que_quero_repetir,
+           ~ sqrt(.x))
+
+
 
 # map para o render
 map(
@@ -41,3 +65,33 @@ map(
     output_file = glue::glue("relatorios_uf/{.x}.html")
   )
 )
+
+
+# alterar o output:
+map(
+  estados_no_sigbm,
+  ~ rmarkdown::render(
+    caminho_rmd,
+    # output_format = word_document(),
+    output_format = prettydoc::html_pretty(theme = "architect"),
+    params = list(estado = .x),
+    output_file = glue::glue("relatorios_uf/{.x}.html")
+  )
+)
+
+
+
+# imprime em PDF!
+pagedown::chrome_print("relatorio-parte-2.html")
+
+caminhos_html <- fs::dir_ls("relatorios_uf/")
+
+caminhos_html_base <- list.files("relatorios_uf", full.names = TRUE)
+
+
+# map(vetor, funcao)
+
+map(caminhos_html, pagedown::chrome_print)
+
+
+
